57086ef29cd6ee9c5416be4e0d7dd38af4576819
Jeremy Hsu <jeremyhsu1@gmail.com>
2021-05-13 19:16:24 -0700

    Form takes subtotal and tip percent

--- SplitBill/ContentView.swift
+++ SplitBill/ContentView.swift
@@ -11,29 +11,44 @@ extension String {
   var isInt: Bool {
     return Int(self) != nil
   }
+  
+  var isFloat: Bool {
+    return Float(self) != nil
+  }
 }
 
 struct ContentView: View {
-  @State var amount: String = ""
+  @State private var subtotal: String = ""
+  @State private var tip: String = ""
+  @State private var total: Float = -1
+  
+  var disableForm: Bool {
+    subtotal.isEmpty || !subtotal.isFloat || tip.isEmpty || !tip.isInt
+  }
   
   var body: some View {
     NavigationView {
       VStack {
         Form {
-          TextField("Amount", text: $amount)
+          TextField("Amount", text: $subtotal)
+          
+          TextField("Tip Percent", text: $tip)
           
           Button(action: {
             // Perform action here
-            print("The user entered \(amount)")
+            print("The user entered \(subtotal)")
+            total = Float(subtotal)! * (1.0 + Float(tip)! / 100)
           }) {
             Text("Submit Form")
           }
-          .disabled(amount.isEmpty || !amount.isInt)
+          .disabled(disableForm)
         }
         .navigationBarTitle("Form")
         
-        Text(amount)
-          .padding()
+        if total > -1 {
+          let formattedTotal: String = String(format: "%.2f", total)
+          Text("Your total is $\(formattedTotal)")
+        }
       }
     }
   }
